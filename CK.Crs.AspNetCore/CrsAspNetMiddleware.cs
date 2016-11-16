125af72a643260041152788838a7dd4b969a1c8d
Cédric Legendre <albundy1385@gmail.com>
2016-11-16 15:53:10 +0100

    Add CK.Crs.AspNetCore

---
+++ CK.Crs.AspNetCore/CrsAspNetMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CK.Crs.Runtime;
+using Microsoft.AspNetCore.Http;
+using CK.Crs;
+
+namespace CK.Crs.AspNetCore
+{
+    // You may need to install the Microsoft.AspNet.Http.Abstractions package into your project
+    class CrsMiddleware 
+    {
+        ICrsHandler _handler;
+        RequestDelegate _next;
+
+        /// <summary>
+        /// Shared options
+        /// </summary>
+        public CrsMiddleware( RequestDelegate next, ICrsHandler handler )
+        {
+            if( handler == null ) throw new ArgumentNullException( nameof( handler ) );
+
+            _next = next;
+            _handler = handler;
+        }
+
+        public async Task Invoke( HttpContext context )
+        {
+            var connectionId = context.Request.Query["c"];
+            var commandRequest = new CommandRequest( context.Request.Path.Value, context.Request.Body, context.User, connectionId );
+            var response = await _handler.ProcessCommandAsync( commandRequest, context.Response.Body );
+            if( response != null )
+            {
+                foreach( var kv in response.Headers )
+                    context.Response.Headers.SetCommaSeparatedValues( kv.Key, kv.Value );
+            }
+            if( response == null && _next != null )
+                await _next.Invoke( context );
+        }
+
+    }
+}
---
+++ CK.Crs.AspNetCore/CrsExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using CK.Core;
+using CK.Crs;
+using CK.Crs.AspNetCore;
+using CK.Crs.Runtime;
+using CK.Crs.Runtime.Execution;
+using Microsoft.AspNetCore.Builder;
+
+namespace Microsoft.AspNetCore
+{
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class CrsExtensions
+    {
+        public static IApplicationBuilder UseCrs( this IApplicationBuilder builder, string routePrefix, IServiceProvider applicationServices, Action<CrsConfiguration> configure = null )
+        {
+            if( routePrefix == null ) throw new ArgumentNullException( nameof( routePrefix ) );
+
+            return builder.Map( routePrefix, ( app ) =>
+            {
+                var crsBuilder = new CrsBuilder( routePrefix, applicationServices);
+                crsBuilder.ApplyDefaultConfigurationOrConfigure( configure );
+
+                var commandReceiver = crsBuilder.Build( applicationServices );
+
+                app.UseMiddleware<CrsMiddleware>( commandReceiver );
+            } );
+        }
+
+        class CrsBuilder : CrsReceiverBuilder
+        {
+            IServiceProvider _services;
+            public CrsBuilder( string routePrefix, IServiceProvider services ) : base( routePrefix, services.GetService<ICommandRegistry>() )
+            {
+                _services = services;
+            }
+
+            protected override void ConfigureDefaultPipeline( ICrsConfiguration config )
+            {
+                config.Pipeline
+                    .Clear()
+                    .UseMetaComponent()
+                    .UseCommandRouter()
+                    .UseJsonCommandBuilder()
+                    .UseAmbientValuesValidator()
+                    .UseFilters()
+                    .UseTaskBasedCommandExecutor( Config.TraitContext )
+                    .UseSyncCommandExecutor()
+                    .UseJsonCommandWriter();
+
+                config.ExternalComponents.CommandScheduler = new InMemoryScheduler( config, _services );
+            }
+        }
+    }
+}
