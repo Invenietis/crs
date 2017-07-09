876aac7f8ad4767737ec8ff0a59ff3f7b61e7280
Cédric Legendre <albundy1385@gmail.com>
2017-07-09 18:00:28 +0200

    Add Executor app sample (empty)

---
+++ samples/CK.Crs.Samples.ExecutorApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CK.Crs.Samples.ExecutorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
