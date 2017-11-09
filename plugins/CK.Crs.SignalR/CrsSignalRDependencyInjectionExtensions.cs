8168b00ebdff50bfd9ba5b839e711db1125e5f34
Cédric Legendre <albundy1385@gmail.com>
2017-11-09 09:21:19 +0100

    Add CrsHubPath configuration.

--- plugins/CK.Crs.SignalR/CrsSignalRDependencyInjectionExtensions.cs
+++ plugins/CK.Crs.SignalR/CrsSignalRDependencyInjectionExtensions.cs
@@ -8,14 +8,18 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
+
     public static class CrsSignalRDependencyInjectionExtensions
     {
-        public static ICrsCoreBuilder AddSignalR( this ICrsCoreBuilder builder )
+        public static ICrsCoreBuilder AddSignalR( this ICrsCoreBuilder builder, Action<CrsSignalROptions> configure = null )
         {
             builder.Services.AddSignalR();
             builder.AddDispatcher<SignalRDispatcher>( CrsSignalRContextExtensions.CallerIdProtocol );
             builder.Services.AddSingleton<SignalRDispatcher>();
             builder.Services.AddSingleton<IStartupFilter, CrsSignalRStartupFilter>();
+
+            if( configure == null ) configure = new Action<CrsSignalROptions>( _ => { } );
+            builder.Services.Configure( configure );
             return builder;
         }
     }
---
+++ plugins/CK.Crs.SignalR/CrsSignalROptions.cs
@@ -0,0 +1,18 @@
+namespace CK.Crs.SignalR
+{
+    /// <summary>
+    /// Options for CRS SignalR hub
+    /// </summary>
+    public class CrsSignalROptions
+    {
+        public CrsSignalROptions()
+        {
+            CrsHubPath = "crs";
+        }
+
+        /// <summary>
+        /// Gets or sets the crs hub path. Defaults to crs.
+        /// </summary>
+        public string CrsHubPath { get; set; }
+    }
+}
