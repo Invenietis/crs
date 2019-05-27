8de7558d5aa4f8cbc0bdd114d025e4a0f06c89ba
Franck Bontemps <franck.bontemps@signature-code.com>
2019-05-27 17:17:49 +0200

    [DONE] added exception details

--- plugins/CK.Crs.Dispatcher/CrsCommandDispatcher.cs
+++ plugins/CK.Crs.Dispatcher/CrsCommandDispatcher.cs
@@ -16,7 +16,7 @@ namespace CK.Crs
         public Task<Response> Send( Guid commandId, object command, Type commandType, CallerId callerId )
         {
             var commandModel = _service.GetService<ICommandRegistry>().GetCommandByName( new CommandName( commandType ) );
-            if( commandModel == null ) throw new ArgumentException( "Command not found" );
+            if( commandModel == null ) throw new ArgumentException( $"Command {commandType} not found" );
 
             var context = new DispatcherCommandContextNoWait<object>( commandId, command, commandModel, callerId, _service ); ;
             return context.Receive();
@@ -25,7 +25,7 @@ namespace CK.Crs
         public Task<Response> Send<TCommand>( Guid commandId, TCommand command, CallerId callerId )
         {
             var commandModel = _service.GetService<ICommandRegistry>().GetCommandByName( new CommandName( typeof( TCommand ) ) );
-            if( commandModel == null ) throw new ArgumentException( "Command not found" );
+            if( commandModel == null ) throw new ArgumentException( $"Command {typeof( TCommand )} not found" );
 
             var context = new DispatcherCommandContextNoWait<TCommand>( commandId, command, commandModel, callerId, _service ); ;
             return context.Receive();
@@ -34,7 +34,7 @@ namespace CK.Crs
         public Task<TResult> SendAndWaitResult<TCommand, TResult>( Guid commandId, TCommand command, CallerId callerId ) where TCommand : ICommand<TResult>
         {
             var commandModel = _service.GetService<ICommandRegistry>().GetCommandByName( new CommandName( typeof( TCommand ) ) );
-            if( commandModel == null ) throw new ArgumentException( "Command not found" );
+            if( commandModel == null ) throw new ArgumentException( $"Command {typeof( TCommand )} not found" );
 
             var context = new DispatcherCommandContext<TCommand, TResult>( commandId, command, commandModel, callerId, _service ); ;
             return context.Receive();
