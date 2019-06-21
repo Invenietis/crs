1b509a2d248bdfba185c46483dfc6b78053de901
Benjamin Crosnier <benjamin.crosnier@signature-code.com>
2019-06-21 10:57:32 +0200

    Move ResponseProxy to a new file

--- js/crs-client-signalr/src/SignalrResponseReceiver.ts
+++ js/crs-client-signalr/src/SignalrResponseReceiver.ts
@@ -1,44 +1,7 @@
 import { ResponseReceiver, CommandResponse, ResponseType } from "@signature/crs-client";
 import { LogLevel } from '@aspnet/signalr';
 import { HubConnectionManager } from "./HubConnectionManager";
-
-class ResponseProxy {
-    private responseType?: ResponseType;
-    private result?: any;
-    private error?: any;
-    private resolve?: Function;
-    private reject?: Function;
-
-    constructor(
-        private readonly commandId: string
-        ) {
-    }
-
-    public setResponse(responseType: ResponseType, result: any) {
-        this.responseType = responseType;
-        this.result = result;
-        this.checkResolve();
-    }
-
-    public setError(reason: any) {
-        this.error = reason;
-        this.checkResolve();
-    }
-
-    public setResolver(resolve: Function, reject: Function) {
-        this.resolve = resolve;
-        this.reject = reject;
-        this.checkResolve();
-    }
-
-    private checkResolve() {
-        if(this.resolve && this.responseType) {
-            this.resolve(this.responseType, this.result);
-        } else if(this.reject && this.error) {
-            this.reject(this.error);
-        }
-    }
-}
+import { ResponseProxy } from "./ResponseProxy";
 
 export class SignalrResponseReceiver implements ResponseReceiver {
     private readonly hubConnectionManager: HubConnectionManager;
