4735403e3e41ff662de47fe4f8c5727485ce9328
Olivier Spinelli <olivier.spinelli@signature-code.com>
2020-04-12 08:00:27 +0200

    Fix NPM project handling.

--- CodeCakeBuilder/npm/AngularWorkspace.cs
+++ CodeCakeBuilder/npm/AngularWorkspace.cs
@@ -1,5 +1,7 @@
+using Cake.Common.Diagnostics;
 using CK.Text;
 using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -34,14 +36,37 @@ namespace CodeCake
             foreach( var project in jsonProject.Properties() )
             {
                 var projectPath = project.Value["root"].ToString();
-                var outputPath = project.Value["architect"]?["build"]?["options"]?["outputPath"]?.Value<string>()
-                    ?? projectPath;
+                var options = project.Value["architect"]["build"]["options"];
+                string outputPathJson = options["outputPath"]?.Value<string>();
+                bool havePath = outputPathJson != null;
+                string ngPackagePath = options["project"]?.Value<string>();
+                bool haveNgPackageJson = ngPackagePath != null;
+                if( havePath && haveNgPackageJson ) throw new NotImplementedException();//I don't know what to do in this case.
+                NormalizedPath outputPath;
+                NormalizedPath ngPackagePathFullPath = path.Combine( ngPackagePath );
+                if( haveNgPackageJson )
+                {
+                    JObject ngPackage = JObject.Parse( File.ReadAllText( ngPackagePathFullPath ) );
+                    string dest = ngPackage["dest"]?.Value<string>();
+                    if( dest == null ) throw new InvalidDataException( "ng package does not contain dest path." );
+                    outputPath = ngPackagePathFullPath.RemoveLastPart().Combine( dest ).ResolveDots();
+                }
+                else if( havePath )
+                {
+                    outputPath = path.Combine( outputPathJson );
+                }
+                else
+                {
+                    globalInfo.Cake.Warning( $"No path found for angular project '{path}'." );
+                    outputPath = path.Combine( projectPath );
+                }
+
                 projects.Add(
                     NPMPublishedProject.Create(
                         globalInfo,
                         npmSolution,
                         path.Combine( projectPath ),
-                        path.Combine( outputPath )
+                        outputPath
                     )
                 );
             }
